d31455aee96c2956ae06faec533507cda27ac5a7
ZhuTechLLC <taotao.zhu1985@gmail.com>
2025-06-26 21:59:09 -0400

    fix: 增加 base 路径以适配 GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  base: '/US_Stock_Investing_Handbook/',
+  outDir: 'docs',
   integrations: [mdx()],
 });
